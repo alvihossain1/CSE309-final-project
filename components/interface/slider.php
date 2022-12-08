89965c7323c4099f44a8852d983387da23cc5e00
alvihossain1 <2020780@iub.edu.bd>
2022-12-08 17:56:29 +0600

    new updates 2

--- components/interface/slider.php
+++ components/interface/slider.php
@@ -7,32 +7,46 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
+                <!-- <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
                             url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/horse-movies-1553108662.jpg);">
-                </div>
+                </div> -->
+                <video autoplay muted class="w-100 carousel-video">
+                    <source src="video03.mp4" type="video/mp4">
+                </video>
+                <div class="gradient position-absolute start-0 top-0 end-0 bottom-0"></div>
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Cinematic Shows</h5>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolorum dolores explicabo id aut quibusdam?</p>
                 </div>
             </div>
-            <div class="carousel-item">
-                <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
-                            url(https://img.freepik.com/free-vector/movie-theater-cinema-hall-with-wide-screen-seats_107791-16264.jpg?w=2000);">
-                </div>
+            <div class="carousel-item active">
+                <!-- <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
+                            url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/horse-movies-1553108662.jpg);">
+                </div> -->
+                <video autoplay muted class="w-100 carousel-video">
+                    <source src="video01.mp4" type="video/mp4">
+                </video>
+                <div class="gradient position-absolute start-0 top-0 end-0 bottom-0"></div>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Now Showing</h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id fuga ut sequi distinctio eligendi at?</p>
+                    <h5>Cinematic Shows</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolorum dolores explicabo id aut quibusdam?</p>
                 </div>
             </div>
-            <div class="carousel-item">
-                <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
-                            url(https://img.freepik.com/premium-vector/cinema-landing-page-movie-concert-hall-with-rows-armchairs-luminous-screen-interior_9209-6258.jpg?w=2000);">
-                </div>
+            <div class="carousel-item active">
+                <!-- <div class="carousel-image" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), 
+                            url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/horse-movies-1553108662.jpg);">
+                </div> -->
+                <video autoplay muted class="w-100 carousel-video">
+                    <source src="video02.mp4" type="video/mp4">
+                </video>
+                <div class="gradient position-absolute start-0 top-0 end-0 bottom-0"></div>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Get Your Tickets</h5>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laborum cumque vitae fuga aliquid. Quisquam?</p>
+                    <h5>Cinematic Shows</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolorum dolores explicabo id aut quibusdam?</p>
                 </div>
             </div>
+            
+
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
