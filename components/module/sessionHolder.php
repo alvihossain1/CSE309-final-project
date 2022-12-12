a5e3a0842d91be319f2470550653e2f04484bede
alvihossain1 <2020780@iub.edu.bd>
2022-12-12 23:41:10 +0600

    final updates last

--- components/module/sessionHolder.php
+++ components/module/sessionHolder.php
@@ -3,6 +3,7 @@ if(isset($_POST['logout-submit'])){
     if(isset($_SESSION['loggedInUser']) || isset($_SESSION['userEmail'])){
         unset($_SESSION['loggedInUser']);
         unset($_SESSION['userEmail']);
+        header('Location: index.php');
     }
 }
 ?>
